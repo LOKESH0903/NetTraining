c5b236dcf871687206ae937a188a3e24c8c745ee
LOKESHCHENNAMSETTI <chaitanya_mca09@hotmail.com>
2025-03-10 19:45:04 +0530

    Switch

--- Training/Training/MathsCalculations.cs
+++ Training/Training/MathsCalculations.cs
@@ -8,6 +8,67 @@ namespace Training
 {
     class MathsCalculations
     {
+
+        public int MathsResult(int a, int b, int type )
+        {
+            int result = 0;
+
+    
+            switch(type)
+            {
+                case 1:
+                    result = Addition(a, b);
+                    break;
+                case 2:
+                    result = Subtraction(a, b);
+                    break;
+                case 3:
+                    result = Multiplication(a, b);
+                    break;
+                case 4:
+                    result = Division(a, b);
+                    break;
+                default:
+                    result = 0;
+                    break;
+            }
+
+       
+
+
+            //if(type == 1)
+            //{
+            //    result = Addition(a, b);
+            //}
+            //else if (type == 2)
+            //{
+            //    result = Subtraction(a, b);
+            //}
+            //else if (type == 3)
+            //{
+            //    result = Multiplication(a, b);
+            //}
+            //else if (type == 4)
+            //{
+            //    result = Division(a, b);
+            //}
+
+            return result;
+
+
+
+
+
+            //switch(type)
+            //{
+            //    case 1:
+            //        return Addition(a, b);
+
+            //    default:
+            //        return 0; 
+            //}
+        }
+
      
         public int Addition(int a, int b)
         {
@@ -21,10 +82,11 @@ namespace Training
 
         public int Multiplication(int a, int b)
         {
-            return a * b;
+            int c =  a * b;
+            return c;
         }
 
-        public double Division(double a, double b)
+        public int Division(int a, int b)
         {
             return a / b;
         }
--- Training/Training/Program.cs
+++ Training/Training/Program.cs
@@ -7,23 +7,66 @@ namespace Training
         static void Main(string[] args)
         {
             MathsCalculations mat = new MathsCalculations();
+            int result = mat.MathsResult(30, 10, 2);
 
-            int additionResult = mat.Addition(1, 2);
-
-            int SubtractionResult = mat.Subtraction(1, 2);
-
-            int MultipResult = mat.Multiplication(1, 2);
-
-            double DivisionResult = mat.Division(1, 2);
+            Console.WriteLine("The result " + result.ToString());
 
 
-            Console.WriteLine("The addition value is : " + additionResult.ToString());
+            /*
+         switch(expression)
+       {
+           case 0:
+               logic 
+           break
+           case 1:
+               logic 
+           break;
 
-            Console.WriteLine("The addition value is : " + SubtractionResult.ToString());
+          default:
+           break;
+       }
+        */
+            //string xyz = "";
+            //string abc = "hi";
+            //switch (abc)
+            //{
+            //    case "hello":
+            //        xyz = "hello";
+            //        break;
+            //    case "hi":
+            //        xyz = "hi";
+            //        break;
+            //    default:
+            //        xyz = "bye";
+            //        break;
+            //}
 
-            Console.WriteLine("The addition value is : " + MultipResult.ToString());
+            //Console.WriteLine("Result based on selected Switch  " + xyz);
 
-            Console.WriteLine("The addition value is : " + DivisionResult.ToString());
+            /*
+             1 Add
+             2 Sub  
+             3 Mul
+             4 Div
+             */
+
+
+            //int additionResult = mat.Addition(1, 2);
+
+            //int SubtractionResult = mat.Subtraction(1, 2);
+
+            //int MultipResult = mat.Multiplication(1, 2);
+
+            //double DivisionResult = mat.Division(1, 2);
+
+
+            //Console.WriteLine("The addition value is : " + additionResult.ToString());
+
+            //Console.WriteLine("The addition value is : " + SubtractionResult.ToString());
+
+            //Console.WriteLine("The addition value is : " + MultipResult.ToString());
+
+            //Console.WriteLine("The addition value is : " + DivisionResult.ToString());
 
             Console.ReadLine();
         }
