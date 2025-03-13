0c6bdd4c68eff39024f625d815322deaa8b4678d
LOKESHCHENNAMSETTI <chaitanya_mca09@hotmail.com>
2025-03-13 18:08:01 +0530

    enum

---
+++ Training/Training/TestClass.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Training
+{
+    public class TestClass
+    {
+        int number = 10;
+        public int StudentId { get; set; }
+
+        public string StudentName { get ; set; }
+    }
+}
